6e1b420473078e768a98aca54fa1e06664b740d3
Mouaad <boukiaou.mouaad@gmail.com>
2020-01-26 18:46:28 +0100

    last commit

--- src/templates/project.js
+++ src/templates/project.js
@@ -2,13 +2,26 @@ import React from "react"
 import { graphql } from "gatsby"
 import ProjectLayout from "../Layouts/ProjectLayout"
 import Seo from "../components/seo"
-
+import PageTransition from "gatsby-plugin-page-transitions"
 export default function Project({ data }) {
   return (
-    <div>
+    <PageTransition
+      defaultStyle={{
+        transition: "left 500ms cubic-bezier(0.47, 0, 0.75, 0.72)",
+        left: "100%",
+        position: "absolute",
+        width: "100%",
+      }}
+      transitionStyles={{
+        entering: { left: "0%" },
+        entered: { left: "0%" },
+        exiting: { left: "100%" },
+      }}
+      transitionTime={100}
+    >
       <Seo title="About" />
       <ProjectLayout data={data} />
-    </div>
+    </PageTransition>
   )
 }
 
--- src/templates/projectCategory.js
+++ src/templates/projectCategory.js
@@ -2,14 +2,27 @@ import React from "react"
 import Header from "../components/Header"
 import ProjectCategoryLayout from "../Layouts/ProjectCategoryLayout"
 import Seo from "../components/seo"
-
+import PageTransition from "gatsby-plugin-page-transitions"
 export default function ProjectCategory({ data }) {
   return (
-    <div>
+    <PageTransition
+      defaultStyle={{
+        transition: "left 500ms cubic-bezier(0.47, 0, 0.75, 0.72)",
+        left: "100%",
+        position: "absolute",
+        width: "100%",
+      }}
+      transitionStyles={{
+        entering: { left: "0%" },
+        entered: { left: "0%" },
+        exiting: { left: "100%" },
+      }}
+      transitionTime={100}
+    >
       <Seo title="About" />
       <Header />
       <ProjectCategoryLayout data={data} />
-    </div>
+    </PageTransition>
   )
 }
 
