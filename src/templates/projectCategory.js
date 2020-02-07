2fdc1d8f0c346ebf54493bffab236ae110cdb7dc
Mouaad Bk <boukiaou.mouaad@gmail.com>
2020-02-07 21:50:59 +0100

    scrolling fixed

--- src/templates/projectCategory.js
+++ src/templates/projectCategory.js
@@ -23,7 +23,9 @@ export default function ProjectCategory({ data }) {
 
 export const query = graphql`
   query($title: String) {
-    allSanityProject {
+    allSanityProject(
+      filter: { categories: { elemMatch: { title: { eq: $title } } } }
+    ) {
       edges {
         node {
           title
